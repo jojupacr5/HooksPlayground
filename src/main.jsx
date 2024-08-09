b9481cf5ed188f6c21849e6010fccd32bbda7d7d
jojupacr5 <joseph.paucar@gmail.com>
2024-08-09 17:57:02 -0500

    Aprendiendo sobre el useReducer entendiendo lo que es una funcion pura y que el reducer siempre debe mandar un nuevo state

--- src/main.jsx
+++ src/main.jsx
@@ -12,10 +12,12 @@ import { createRoot } from 'react-dom/client'
 // import { CallbackHook } from './05-memos/CallbackHook'
 import { Padre } from './07-tarea-memo/Padre'
 
+import './08-useReducer/intro-reducer'
+
 import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-    <Padre />
-  // </StrictMode>,
-)
+// createRoot(document.getElementById('root')).render(
+//   // <StrictMode>
+//     <Padre />
+//   // </StrictMode>,
+// )
