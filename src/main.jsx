beae168d6177c8740e9a9bf898af3cf7705c07ad
jojupacr5 <joseph.paucar@gmail.com>
2024-08-08 21:01:23 -0500

    comenzando com el usememo

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,13 @@ import { createRoot } from 'react-dom/client'
 // import { CounterWithCoustomHook } from './01-useState/CounterWithCoustomHook'
 // import { SimpleForm } from './02-useEffect/SimpleForm'
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
-import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+// import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+import { FocusScreen } from './04-useRef/FocusScreen'
 
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <MultipleCustomHooks />
-  </StrictMode>,
+  // <StrictMode>
+    <FocusScreen />
+  // </StrictMode>,
 )
