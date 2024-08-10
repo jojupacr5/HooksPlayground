e245fe17a85a0d19902403bd710c9a3330b2def6
jojupacr5 <joseph.paucar@gmail.com>
2024-08-10 01:01:47 -0500

    Se creo AppTodos para utilizar y aprender sobre useReducer

--- src/main.jsx
+++ src/main.jsx
@@ -10,14 +10,14 @@ import { createRoot } from 'react-dom/client'
 // import { Memorize } from './05-memos/Memorize'
 // import { MemoHook } from './05-memos/MemoHook'
 // import { CallbackHook } from './05-memos/CallbackHook'
-import { Padre } from './07-tarea-memo/Padre'
-
-import './08-useReducer/intro-reducer'
+// import { Padre } from './07-tarea-memo/Padre'
+// import './08-useReducer/intro-reducer'
+import { TodoApp } from './08-useReducer/TodoApp'
 
 import './index.css'
 
-// createRoot(document.getElementById('root')).render(
-//   // <StrictMode>
-//     <Padre />
-//   // </StrictMode>,
-// )
+createRoot(document.getElementById('root')).render(
+  // <StrictMode>
+    <TodoApp />
+  // </StrictMode>,
+)
